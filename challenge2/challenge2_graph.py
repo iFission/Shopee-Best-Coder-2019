91b772213f164e372388c2b5978731acfcbd0213
iFission <alexwwj@icloud.com>
2020-05-19 23:52:25 +0800

    feat: add multiprocessing

--- challenge2/challenge2_graph.py
+++ challenge2/challenge2_graph.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tqdm import tqdm
 
 #%%
-orders = pd.read_csv("orders.csv")
+orders = pd.read_csv("orders.csv").astype(str)
 orders = orders.set_index("orderid", drop=False)
 orders['is_fraud'] = [0 for i in range(orders.shape[0])]
 orders.head()
@@ -13,11 +13,11 @@ bank_accounts = pd.read_csv("bank_accounts.csv").astype(str)
 bank_accounts = bank_accounts.set_index("userid", drop=False)
 bank_accounts.head()
 #%%
-credit_cards = pd.read_csv("credit_cards.csv")
+credit_cards = pd.read_csv("credit_cards.csv").astype(str)
 credit_cards = credit_cards.set_index("userid", drop=False)
 credit_cards.head()
 #%%
-devices = pd.read_csv("devices.csv")
+devices = pd.read_csv("devices.csv").astype(str)
 devices = devices.set_index("userid", drop=False)
 devices.head()
 
@@ -26,82 +26,99 @@ from iteration_utilities import duplicates, unique_everseen
 import itertools
 import networkx as nx
 
-# find duplicate entries with many userid
-bank_account_ls = bank_accounts['bank_account'].to_list()
-info_with_multiple_userid_ls = list(
-    unique_everseen(duplicates(bank_account_ls)))
 
-#%%
-# construct new df, to cut down on the number of rows
-info_with_multiple_userid = pd.DataFrame(info_with_multiple_userid_ls,
-                                         columns=["info"])
-info_with_multiple_userid.head()
+def find_repeated_info(df):
 
-merged = pd.merge(info_with_multiple_userid,
-                  bank_accounts,
-                  how="left",
-                  left_on="info",
-                  right_on=bank_accounts.columns[1]).drop(
-                      bank_accounts.columns[1], axis=1)
+    global merged
+    global info_with_multiple_userid_ls
+
+    # find duplicate entries with many userid
+    df_colunm1_ls = df[df.columns[1]].to_list()
+    info_with_multiple_userid_ls = list(
+        unique_everseen(duplicates(df_colunm1_ls)))
+
+    # construct new df, to cut down on the number of rows
+    info_with_multiple_userid = pd.DataFrame(info_with_multiple_userid_ls,
+                                             columns=["info"])
+    info_with_multiple_userid.head()
+
+    merged = pd.merge(info_with_multiple_userid,
+                      df,
+                      how="left",
+                      left_on="info",
+                      right_on=df.columns[1]).drop(df.columns[1], axis=1)
+
+
+def build_graph(start, end):
+    # build graph
+    global graph
+    # using the info as edge, associated userid are linked nodes
+    for info_repeated in tqdm(info_with_multiple_userid_ls[start:end]):
+        # get the associated userid
+        userid_ls = merged[merged['info'].isin([info_repeated
+                                                ])]['userid'].tolist()
+
+        for userid_1, userid_2 in itertools.combinations(userid_ls, 2):
+            graph.add_edge(userid_1, userid_2)
+
+    # return graph
+
+
+from multiprocessing import Process
+
+
+def split_build_graph(split_count=12):
+
+    split_size = len(info_with_multiple_userid_ls) // split_count
+    threads = []
+    for i in range(split_count):
+        # determine the indices of the list this thread will handle
+        start = i * split_size
+        # special case on the last chunk to account for uneven splits
+        end = None if i + 1 == split_count else (i + 1) * split_size
+
+        # create the thread
+        threads.append(Process(target=build_graph, args=(start, end)))
+        threads[-1].start()  # start the thread we just created
+
+    # wait for all threads to finish
+    for t in threads:
+        t.join()
+
+    print(graph.number_of_edges())
+    print(graph.number_of_nodes())
+
 
 #%%
 # build graph
-G = nx.Graph()
-# using the info as edge, associated userid are linked nodes
-for userid_repeated in tqdm(info_with_multiple_userid_ls):
-    # get the associated userid
-    userid_ls = merged[merged['info'].isin([userid_repeated
-                                            ])]['userid'].tolist()
+graph = nx.Graph()
+merged = 0
+info_with_multiple_userid_ls = 0
 
-    for userid_1, userid_2 in itertools.combinations(userid_ls, 2):
-        G.add_edge(userid_1, userid_2)
+for df in [bank_accounts]:
+    find_repeated_info(df)
+    split_build_graph(split_count=15)
+# graph = build_graph(devices, graph)
+# graph = build_graph(credit_cards, graph)
 
 #%%
 # check graph statistics
-print(G.number_of_edges())
-print(G.number_of_nodes())
+
 #%%
 # iterate through all orders
-# check if there's overlap of buyer_userid and seller_userid in device, credit_card, bank_account
-# check overlap by filtering each df with user_id, check device column, convert to list, then convert to set, then check overlap, then convert to list, then count the length of list
-
-orders_sub = orders
+orders_original = orders
 for orderid, buyer_userid, seller_userid in tqdm(
-        zip(orders_sub['orderid'], orders_sub['buyer_userid'],
-            orders_sub['seller_userid'])):
+        zip(orders_original['orderid'], orders_original['buyer_userid'],
+            orders_original['seller_userid'])):
     device_count, bank_account_count, credit_card_count = 0, 0, 0
 
     try:
-        device_count = len(
-            list(
-                set(devices.loc[buyer_userid]['device'].to_list())
-                & set(devices.loc[seller_userid]['device'].to_list())))
-    except:
-        pass
-
-    try:
-        bank_account_count = len(
-            list(
-                set(bank_accounts.loc[buyer_userid]['bank_account'].to_list())
-                & set(bank_accounts.loc[seller_userid]
-                      ['bank_account'].to_list())))
-    except:
-        pass
-
-    try:
-        credit_card_count = len(
-            list(
-                set(credit_card.loc[buyer_userid]['credit_card'].to_list())
-                & set(credit_card.loc[seller_userid]['credit_card'].to_list()))
-        )
-    except:
-        pass
-
-    if sum([device_count, bank_account_count, credit_card_count]) > 0:
-        print(orderid)
-
+        # find a path between buy and seller
+        length = nx.shortest_path_length(graph, buyer_userid, seller_userid)
         orders.at[orderid, "is_fraud"] = 1
-    # print(list1, list2)
+        print(f"\norderid: {orderid}, length: {length}")
+    except Exception:
+        pass
 
 #%%
 orders.drop('buyer_userid', axis=1, inplace=True)
