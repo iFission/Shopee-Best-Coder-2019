fb678c0b778b4b0be023aaec0dcdb48d85b24534
iFission <alexwwj@icloud.com>
2020-05-19 17:22:10 +0800

    feat: add code from competition

---
+++ challenge1/challenge1_fuzzy.py
@@ -0,0 +1,36 @@
+#%%
+import pandas as pd
+from tqdm import tqdm
+from fuzzywuzzy import fuzz
+
+#%%
+keyword = pd.read_csv("Keyword_spam_question.csv")
+keyword.head()
+keyword['groups_found'] = [[] for i in range(keyword.shape[0])]
+
+#%%
+extra = pd.read_csv("Extra Material 2 - keyword list_with substring.csv")
+extra.head()
+category_index_name = {}
+for index in range(len(extra['Keywords'])):
+    category_index_name[index] = extra['Keywords'][index].split(", ")
+
+#%%
+# keyword = keyword.sample(100)
+for keyword_index, keyword_description in tqdm(
+        zip(keyword['index'], keyword['name'])):
+    assigned_category = []
+    for category_index, category_list in category_index_name.items():
+        for category in category_list:
+            if fuzz.partial_ratio(category, keyword_description) >= 70:
+                # print(keyword_description, category)
+                # print(category_index)
+                assigned_category.append(category_index)
+        # print(index, category_list)
+    # print(assigned_category)
+    keyword.at[keyword_index, "groups_found"] = assigned_category
+#%%
+keyword.drop('name', axis=1, inplace=True)
+import datetime
+now = str(datetime.datetime.now()).replace('-', '')
+keyword.to_csv(f'submission_text_{now}.csv', index=False)
---
+++ challenge2/challenge2.py
@@ -0,0 +1,74 @@
+ #%%
+import pandas as pd
+from tqdm import tqdm
+
+#%%
+orders = pd.read_csv("orders.csv")
+orders = orders.set_index("orderid", drop=False)
+orders['is_fraud'] = [0 for i in range(orders.shape[0])]
+orders.head()
+
+#%%
+bank_accounts = pd.read_csv("bank_accounts.csv")
+bank_accounts = bank_accounts.set_index("userid", drop=False)
+bank_accounts.head()
+#%%
+credit_cards = pd.read_csv("credit_cards.csv")
+credit_cards = credit_cards.set_index("userid", drop=False)
+credit_cards.head()
+#%%
+devices = pd.read_csv("devices.csv")
+devices = devices.set_index("userid", drop=False)
+devices.head()
+#%%
+len(
+    list(
+        set(devices.loc[47388162]['device'].to_list())
+        & set(devices.loc[20822974]['device'].to_list())))
+#%%
+
+orders_sub = orders
+for orderid, buyer_userid, seller_userid in tqdm(
+        zip(orders_sub['orderid'], orders_sub['buyer_userid'],
+            orders_sub['seller_userid'])):
+    device_count, bank_account_count, credit_card_count = 0, 0, 0
+
+    try:
+        device_count = len(
+            list(
+                set(devices.loc[buyer_userid]['device'].to_list())
+                & set(devices.loc[seller_userid]['device'].to_list())))
+    except:
+        pass
+
+    try:
+        bank_account_count = len(
+            list(
+                set(bank_accounts.loc[buyer_userid]['bank_account'].to_list())
+                & set(bank_accounts.loc[seller_userid]
+                      ['bank_account'].to_list())))
+    except:
+        pass
+
+    try:
+        credit_card_count = len(
+            list(
+                set(credit_card.loc[buyer_userid]['credit_card'].to_list())
+                & set(credit_card.loc[seller_userid]['credit_card'].to_list()))
+        )
+    except:
+        pass
+
+    if sum([device_count, bank_account_count, credit_card_count]) > 0:
+        print(orderid)
+
+        orders.at[orderid, "is_fraud"] = 1
+    # print(list1, list2)
+
+#%%
+orders.drop('buyer_userid', axis=1, inplace=True)
+orders.drop('seller_userid', axis=1, inplace=True)
+print(orders.head())
+import datetime
+now = str(datetime.datetime.now()).replace('-', '')
+orders.to_csv(f'submission_text_{now}.csv', index=False)
